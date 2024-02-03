2a7cff45aeecca150c3432b3a3606066ea089a96
Jeison <j.stanleyz.c0619@gmail.com>
2024-02-03 04:09:03 -0500

    Se agrega la seccion de desayuno en alimentacion saludable

--- src/app/heal-food/heal-food.component.ts
+++ src/app/heal-food/heal-food.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { ImageModule } from 'primeng/image';
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
+import { TabViewModule } from 'primeng/tabview';
 
 @Component({
   selector: 'app-heal-food',
   standalone: true,
-  imports: [],
+  imports: [ ButtonModule, DividerModule, ImageModule, AnimateOnScrollModule, TabViewModule ],
   templateUrl: './heal-food.component.html',
   styleUrl: './heal-food.component.css'
 })
