c4003d8116b14c64372d20d7cec8945c86a08c54
Jeison <j.stanleyz.c0619@gmail.com>
2024-02-06 06:20:34 -0500

    Se actualiza comida saludable yb se comienza con el login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,70 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, RouterModule } from '@angular/router';
 import { TabMenuModule } from 'primeng/tabmenu';
 import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import usersList from '../assets/users/users.json';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TabMenuModule, ButtonModule],
+  imports: [RouterOutlet, TabMenuModule, ButtonModule, DialogModule, InputTextModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent{
-  title = 'siemprefit';
-  activeItem: any;
+export class AppComponent implements OnInit{
 
+  constructor(private router:Router) {
+    
+  }
+
+  ngOnInit(): void {
+    if(sessionStorage.getItem("loged") == "true"){
+      this.loged = true;
+    }
+
+    if(sessionStorage.getItem("rol") == "Admin"){
+      this.admin = true;
+    }
+  }
+
+  title = 'siemprefit';
+  visible: boolean = false;
+  users: any = usersList;
+
+  cedula!: string;
+  token!: string;
+
+  loged!: boolean;
+  admin!: boolean;
+
+  showLogin(){
+    this.visible = true;
+  }
+
+  closeLogin(){
+    this.visible = false;
+  }
+
+  login(){
+    this.visible = false;
+    sessionStorage.clear();
+    for(const us of this.users){
+      if(us.cedula == this.cedula && us.token == this.token){
+        sessionStorage.setItem("loged", "true");
+        sessionStorage.setItem("rol", us.rol);
+        break;
+      }
+    } 
+    this.router.navigate(['']);
+    window.location.reload()
+  }
+
+  logout(){
+    sessionStorage.clear();
+    this.router.navigate(['']);
+    window.location.reload()
+  }
 }
